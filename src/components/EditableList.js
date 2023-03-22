74157c44d55cc234bfd5bff19eee2b75df0352f5
afrontend <autoscripts@gmail.com>
2023-03-22 22:11:36 +0900

    Add radio type

--- src/components/EditableList.js
+++ src/components/EditableList.js
@@ -5,12 +5,21 @@ function EditableList({jsonFilename = 'editableList.json'}) {
   const [editableList, setEditableList] = useState([])
   const [resultList, setResultList] = useState({})
 
+  const toResultList = (items) => {
+    const result = {}
+    items.forEach(item => {
+      result[item.id] = item.defaultValue
+    })
+    return result;
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const path = (`${jsonFilename}?` + (new Date()).valueOf())
       const response = await fetch(path);
       const el = await response.json();
       setEditableList(el)
+      setResultList(toResultList(el.items))
     };
     fetchData();
   }, []);
@@ -25,19 +34,47 @@ function EditableList({jsonFilename = 'editableList.json'}) {
       </div>
       {editableList.items?.map(item => {
         return <div key={item.id} className={styles.section}>
-          {item.type === 'checkbox' &&
-            <>
-              <input type={item.type} onChange={e => setResultList({...resultList, [item.id]: e.target.value}) }/>
-              &nbsp;
-              <label htmlFor={item.label} title={item.id}>{item.label}</label>
-            </>
+          {item.type === 'text' &&
+            <div>
+              <label title={item.id}>
+                {item.label}
+                &nbsp;
+                <input type={item.type} value={resultList[item.id]} onChange={e => setResultList({...resultList, [item.id]: e.target.value}) }/>
+                &nbsp;
+              </label>
+            </div>
           }
-          {item.type !== 'checkbox' &&
-            <>
-              <label htmlFor={item.label} title={item.id}>{item.label}</label>
-              &nbsp;
-              <input type={item.type} onChange={e => setResultList({...resultList, [item.id]: e.target.value}) }/>
-            </>
+          {item.type === 'checkbox' &&
+            <div>
+              <label title={item.id}>
+                <input type={item.type} checked={resultList[item.id]} onChange={e => setResultList({...resultList, [item.id]: e.target.checked}) }/>
+                &nbsp;
+                {item.label}
+                &nbsp;
+              </label>
+            </div>
+          }
+          {item.type === 'date' &&
+            <div>
+              <label title={item.id}>
+                {item.label}
+                &nbsp;
+                <input type={item.type} value={resultList[item.id]} onChange={e => setResultList({...resultList, [item.id]: e.target.value}) }/>
+                &nbsp;
+              </label>
+            </div>
+          }
+          {item.type === 'radio' &&
+            item.options.map(option => {
+              return <div key={option}>
+                <label title={item.id}>
+                  <input type={item.type} checked={resultList[item.id] === option} onChange={e => setResultList({...resultList, [item.id]: option}) }/>
+                  &nbsp;
+                  {option}
+                  &nbsp;
+                </label>
+              </div>
+            })
           }
         </div>
       })}
